9227e0a340531411889645974e18c9c553392702
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2021-05-16 18:53:30 +0100

    added web animation with rive
    added web animation with rive

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,1 +1,1 @@
-window.sr=ScrollReveal();
+window.sr=ScrollReveal();let canvas=document.getElementById("canvas");var window_height=window.innerHeight,window_width=window.innerWidth;canvas.width=window_width,canvas.height=window_height,new rive.Rive({src:"https://public.rive.app/community/runtime-files/285-564-nba-2k21-animation-demo.riv",canvas:canvas,autoplay:!0,layout:new rive.Layout({fit:"cover",alignment:"center"})});
--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,23 @@
 
 // JavaScript
-window.sr = ScrollReveal(); 
+window.sr = ScrollReveal();
 
 // sr.reveal('h1', {
 //     delay: 0,
 //     duration: 200,
 //     origin: 'bottom',
-//     distance: '100px' 
+//     distance: '100px'
 // });
+let canvas = document.getElementById("canvas");
+var window_height = window.innerHeight;
+var window_width = window.innerWidth;
+
+canvas.width=window_width;
+canvas.height=window_height;
+
+new rive.Rive({
+  src: 'https://public.rive.app/community/runtime-files/285-564-nba-2k21-animation-demo.riv',
+  canvas: canvas,
+  autoplay: true,
+  layout: new rive.Layout({fit: 'cover', alignment: 'center'}),
+})
